936b411c29c2a2ec306dd94f285c52842cb87004
Karmali <imran.karmali@dxc.com>
2024-09-16 16:15:04 -0500

    Initial Commit

---
+++ src/main/java/com/aa/fos_api/FosApiApplication.java
@@ -0,0 +1,13 @@
+package com.aa.fos_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aa/fos_api/FosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aa.fos_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
